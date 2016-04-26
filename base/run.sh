0de24fde0fa1f64c2acf07010755afaaec8944ad
Shay Bergmann <shaybergmann@gmail.com>
2016-04-26 16:05:57 -0400

    Check if the cache file exists
    before attempting to download. That way in case it's not there yet we
    can print out a friendly message instead of letting rsync spew it's
    error messages all over the place and scaring users.

--- base/run.sh
+++ base/run.sh
@@ -5,10 +5,17 @@ set -e
 caches=${@:1:$(($#-1))}
 
 if [[ -n "$caches" ]]; then
-	dockerize -wait tcp://172.17.0.1:873
-	for cache in $caches; do
-	    rsync -a rsync://172.17.0.1/volume/$cache.tgz . && tar xzf $cache.tgz && rm $cache.tgz || true
-	done
+  dockerize -wait tcp://172.17.0.1:873
+  for cache in $caches; do
+    url=rsync://172.17.0.1/volume/${cache}.tgz
+    if rsync $url &>/dev/null; then
+      rsync -a $url .
+      tar xzf ${cache}.tgz
+      rm ${cache}.tgz
+    else
+      echo $cache is not in the cache. Skipping...
+    fi
+  done
 fi
 
 echo '#!/usr/bin/env bash' > /tmp/script.sh
